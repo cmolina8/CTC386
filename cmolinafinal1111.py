2c5c3d5e9bc965db98f18f94652b22a39a4c9df8
cmolina8 <molics@fusd.net>
2026-01-29 02:14:05 +0000

    Final question 4

---
+++ cmolinafinal1111.py
@@ -0,0 +1,12 @@
+#Ceiry Molina 
+#Final Question 3 
+
+#Function section:
+
+def userinput():
+    num = int(input(print("Please enter a number in range of 1 to 10! \n")))
+    for i in range (num):
+        print("Happy New Year Loser!!")
+
+userinput()
+
