0fac343439e9a5a769730acb79039a5f173ad03e
cmolina8 <molics@fusd.net>
2026-03-19 01:03:39 +0000

    ctc387 midterm question

---
+++ cmolina387_midterm.py
@@ -0,0 +1,11 @@
+#Ceiry Molina 
+#CTC 387 Midterm Question  
+
+x = 0 
+while (x!= 7): 
+    x = float(input("Guess my number if you can loser!Its between 1-10!"))
+    if (x!= 7):
+        print("You didn't guess right, try again loser!") 
+
+print("You guess corectly loser! You won one million dollars!")
+
